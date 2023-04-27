6369fcd4e0a4e70300c818cfae114bd81ef34cc4
midi_cable <bogdan.astronomy@gmail.com>
2023-04-27 17:19:44 +0300

    feat: add header rendering

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,8 @@
-import { renderMainWindow } from "./keyboard-render.js";
+import { renderMainWindow, renderHeader } from "./keyboard-render.js";
 
 function main() {
   renderMainWindow();
+  renderHeader();
 }
 
 main();
--- scripts/keyboard-render.js
+++ scripts/keyboard-render.js
@@ -2,11 +2,30 @@ const renderMainWindow = function() {
   const body = document.body;
   const container = document.createElement('main');
   container.classList.add('main-container');
-  body.appendChild(container);
+  body.prepend(container);
 }
 
-const renderKeyboard = function() {
+const renderHeader = function() {
+  const container = document.querySelector('.main-container');
+  const header = document.createElement('header');
+  const socialLinksContainer = document.createElement('div');
+  const headline = document.createElement('h1');
+
+  socialLinksContainer.classList.add('social-links-container');
+  socialLinksContainer.innerHTML = 
+      '<img class="social-links-container__link" src="./assets/icons/rss-logo.svg" alt="RSS Logo">\n' +
+      '<img class="social-links-container__link" src="./assets/icons/gh-logo.png" alt="GH Logo">';
   
+  headline.classList.add('headline')
+  headline.innerHTML =
+      'Rolling Scopes School <br>' + 
+      'Virtual Keyboard';
+  
+  header.classList.add('header');
+  header.appendChild(socialLinksContainer);
+  header.appendChild(headline);
+
+  container.appendChild(header);
 }
 
-export { renderMainWindow };
+export { renderMainWindow, renderHeader };
